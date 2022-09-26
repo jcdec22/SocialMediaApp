0941cef65e188458ee7f2c9805233c9ea26afbbd
Juan DeCasanova <tuj02238@temple.edu>
2022-09-26 13:43:29 -0400

    Created User model, auth and users routes, and connection to MongoDB

--- index.js
+++ index.js
@@ -4,8 +4,21 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
 
 dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL,()=>{
+    console.log("Connection to MONGODB = Success")
+});
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8800,()=>{
     console.log("Test Backend")
