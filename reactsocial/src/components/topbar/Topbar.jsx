a91c4db7aed577a0d738bebd49e72e7991624cf1
jcdec22 <jcdec22@gmail.com>
2022-10-10 21:06:39 -0400

    Created React App, started components, added icons, and created basic home page.

---
+++ reactsocial/src/components/topbar/Topbar.jsx
@@ -0,0 +1,43 @@
+import "./topbar.css"
+import { Search, Person, Chat, Notifications } from '@mui/icons-material'
+
+export default function Topbar(){
+    return(
+        <div className="topbarContainer">
+            <div className="topbarLeft">
+                <span className="logo">AthleteHub</span>
+            </div>
+            <div className="topbarCenter">
+                <div className="searchBar">
+                    <Search/>
+                    <input placeholder="Search for posts" className="searchInput" />
+                </div>
+            </div>
+            <div className="topbarRight">
+                <div className="topbarLinks">
+                    <span className="topbarLink">HomePage</span>
+                    <span className="topbarLink">Timeline</span>
+                </div>
+                <div className="topbarIcons">
+                    <div className="topbarIconItem">
+                        <Person/>
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                </div>
+                <div className="topbarIcons">
+                    <div className="topbarIconItem">
+                        <Chat/>
+                        <span className="topbarIconBadge">2</span>
+                    </div>
+                </div>
+                <div className="topbarIcons">
+                    <div className="topbarIconItem">
+                        <Notifications/>
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                </div>
+                <img src="/assets/basketballplayer1.jpg" alt="" className="topbarImg" />
+            </div>
+        </div>
+    )
+}
